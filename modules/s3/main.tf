bd27491dcc20a990b7d4527ca0d0ee4f17339ab6
DjkokGit <jamesdennisoh@gmail.com>
2025-11-03 03:42:15 -0600

    im learning terraform in a hard way

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -6,13 +6,3 @@ resource "aws_s3_bucket" "mypracticebucket2310" {
     Environment = "Dev"
   }
 }
-
-resource "aws_instance" "instance" {
-  ami           = "ami-0360c520857e3138f"
-  instance_type = "t3.micro"
-  subnet_id     = "subnet-0b866f5b6e6926c05"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
